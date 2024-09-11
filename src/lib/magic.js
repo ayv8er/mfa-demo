5ffb5c918931d9506cbf76a713fbef0c7d80b795
Richard Oh <richard@magic.link>
2024-09-11 19:33:00 -0400

    updates api key

--- src/lib/magic.js
+++ src/lib/magic.js
@@ -1,6 +1,7 @@
 import { Magic } from "magic-sdk";
 import { OAuthExtension } from '@magic-ext/oauth2';
 
-export const magic = new Magic("pk_live_B1717ED2BD01337F", {
+export const magic = new Magic("pk_live_CD8F5C0C43A10B97", {
     extensions: [new OAuthExtension()],
+    endpoint: 'https://auth.stagef.magic.link'
 });
